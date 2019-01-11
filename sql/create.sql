83d05475234273f75b059b6c4bd2c3941e23281d
yuangui.hu <yuangui.hu@luckincoffee.com>
2019-01-11 09:37:07 +0800

    数据库脚本初始化

---
+++ sql/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `t_test` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `title` varchar(64) DEFAULT NULL,
+  `mem` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
