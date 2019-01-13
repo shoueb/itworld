cdc39b081649a45e763a1ef73d88b259ac92f755
yuangui.hu <yuangui.hu@luckincoffee.com>
2019-01-13 11:27:17 +0800

    task#添加后台模板

--- src/main/java/com/shoueb/itworld/author/mapper/BlogArticleHotMapper.java
+++ src/main/java/com/shoueb/itworld/author/mapper/BlogArticleHotMapper.java
@@ -4,7 +4,15 @@ import com.shoueb.comm.MyMapper;
 import com.shoueb.itworld.author.model.BlogArticleHot;
 
 import java.util.List;
-
+/**
+ * @Description: 热门博客 Mapper
+ * @Author: yuangui.hu
+ * @Date: 2019/1/12 13:22
+ */
 public interface BlogArticleHotMapper extends MyMapper<BlogArticleHot> {
+    /**
+     * 查询首页推荐
+     * @return
+     */
     List<BlogArticleHot> queryHomeRecommendArticle();
 }
