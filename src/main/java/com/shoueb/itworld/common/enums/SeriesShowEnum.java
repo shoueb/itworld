d5646b78c836d5f7809b620d256f0bc586315a90
qw2208161 <591331842@qq.com>
2019-01-27 14:56:45 +0800

    #series

---
+++ src/main/java/com/shoueb/itworld/common/enums/SeriesShowEnum.java
@@ -0,0 +1,54 @@
+package com.shoueb.itworld.common.enums;
+
+/**
+ * @Description:
+ * @Author: yuangui.hu
+ * @Date: 2019/1/13 15:26
+ */
+public enum  SeriesShowEnum{
+    PROSE("0", "散文"),
+    SERIAL("1", "系列文章"),
+    SOLUTION("2", "解决方案"),
+    OPENSOURCE("3", "优秀开源");
+
+
+
+
+
+    private String key;
+    private String value;
+
+    /**
+     * 合同是否有押金枚举
+     * @param key key
+     * @param value 值
+     */
+    SeriesShowEnum(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    /**
+     * 通过key 获取value
+     * @param key  key
+     * @return
+     */
+    public static String getName(Integer key) {
+        if( null == key){
+            return  null;
+        }
+        for (SeriesShowEnum c : SeriesShowEnum.values()) {
+            if (c.getKey().equals(key)) {
+                return c.getValue();
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/shoueb/itworld/web/blog/service/SeriesService.java
@@ -0,0 +1,13 @@
+package com.shoueb.itworld.web.blog.service;
+
+import com.shoueb.itworld.author.model.BlogArticleHot;
+
+import java.util.List;
+
+public interface SeriesService {
+    List<BlogArticleHot> querySeriesRecommendArticle();
+
+    List<BlogArticleHot> queryEditorRecommendArticle(BlogArticleHot blogArticleHot);
+
+    List<BlogArticleHot> querySeriesArticle(BlogArticleHot blogArticleHot);
+}
